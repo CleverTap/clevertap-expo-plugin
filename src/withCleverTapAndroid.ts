d829fd525ea0df47292c3fc980cf26e011080aea
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2025-02-16 02:38:32 +0530

    feat(expo_android_app_build_gradle): inject app build gradle template and gradle properties using withAppBuildGradle and withGradleProperties mod plugins respectively SDK-4413

--- src/withCleverTapAndroid.ts
+++ src/withCleverTapAndroid.ts
@@ -1,8 +1,10 @@
 import { ConfigPlugin } from 'expo/config-plugins';
 import { CleverTapPluginProps } from '../types/types';
 import { withCleverTapAndroidManifest } from './android_config/manifest/withCleverTapAndroidManifest';
+import { withClevertapAndroidAppBuildGradle } from './android_config/gradle/withClevertapAndroidAppBuildGradle';
 
 export const withCleverTapAndroid: ConfigPlugin<CleverTapPluginProps> = (config, props) => {
     config = withCleverTapAndroidManifest(config, props);
+    config = withClevertapAndroidAppBuildGradle(config, props)
     return config;
 }
