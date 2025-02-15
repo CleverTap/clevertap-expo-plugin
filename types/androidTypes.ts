8aed28f7b483569654827fc6afdffd8091f65aac
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2025-02-16 02:30:10 +0530

    feat(expo_android_app_build_gradle): add Dependencies type for injecting dependencies in apps build gradle file SDK-4413

--- types/androidTypes.ts
+++ types/androidTypes.ts
@@ -20,8 +20,45 @@ export interface Features {
     enablePushTemplates?: boolean;
     enableInApp?: boolean;
     enableInbox?: boolean;
-    enableInAppMedia?: boolean;
+    enableMediaForInAppsInbox?: boolean;
     enableInstallReferrer?: boolean;
     enableHmsPush?: boolean;
     enableGoogleAdId?: boolean;
+}
+export interface Dependencies {
+    clevertapCore: {
+        clevertapSdkVersion: string;
+        androidxCoreVersion: string;
+    };
+    pushNotifications: {
+        firebaseMessagingVersion: string;
+    };
+    pushTemplates: {
+        pushTemplatesVersion: string;
+    };
+    inApp: {
+        appCompatVersion: string;
+        fragmentVersion: string;
+    };
+    inbox: {
+        appCompatVersion: string;
+        recyclerViewVersion: string;
+        viewPagerVersion: string;
+        materialVersion: string;
+        glideVersion: string;
+        fragmentVersion: string;
+    };
+    media3: {
+        version: string;
+    };
+    installReferrer: {
+        version: string;
+    };
+    hmsPush: {
+        clevertapHmsSdkVersion: string;
+        hmsPushVersion: string;
+    };
+    googleAdId: {
+        version: string;
+    };
 }
