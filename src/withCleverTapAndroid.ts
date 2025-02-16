20ce19ed31f0ed9a70d75c770ded66d8a44ebc77
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2025-02-16 20:06:00 +0530

    feat(expo_android_root_build_gradle): inject classpath and repository urls SDK-4414

--- src/withCleverTapAndroid.ts
+++ src/withCleverTapAndroid.ts
@@ -2,9 +2,11 @@ import { ConfigPlugin } from 'expo/config-plugins';
 import { CleverTapPluginProps } from '../types/types';
 import { withCleverTapAndroidManifest } from './android_config/manifest/withCleverTapAndroidManifest';
 import { withClevertapAndroidAppBuildGradle } from './android_config/gradle/withClevertapAndroidAppBuildGradle';
+import { withCleverTapRootGradlePlugin } from './android_config/gradle/withCleverTapAndroidAppRootBuildGradle';
 
 export const withCleverTapAndroid: ConfigPlugin<CleverTapPluginProps> = (config, props) => {
     config = withCleverTapAndroidManifest(config, props);
     config = withClevertapAndroidAppBuildGradle(config, props)
+    config = withCleverTapRootGradlePlugin(config, props)
     return config;
 }
